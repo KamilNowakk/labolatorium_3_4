c83090b1c6dbc7914b96ba33d59a37603e1de4e8
KamilNowakk <Kamil.nowak9@interia.pl>
2025-01-06 02:27:36 +0100

    Labolatorium 3 oraz 4

---
+++ app/src/main/java/com/example/labolatorium/screens/home/HomeScreens.kt
@@ -0,0 +1,59 @@
+package com.example.labolatorium.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.labolatorium.MovieRow
+import com.example.labolatorium.navigation.MovieScreens
+
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color.Magenta,
+                elevation = 5.dp
+            ) {
+                Text(text = "Movies")
+            }
+        }
+    ) { it ->
+        Column(modifier = Modifier.padding(it)) {
+            MainContent(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    moviesList: List<String> = listOf(
+        "Avatar",
+        "555",
+        "Harry Potter",
+        "Life",
+        "Lolek",
+        "Bolek",
+        "Krecik",
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(moviesList) { movie ->
+                MovieRow(movie = movie) {
+                    navController.navigate(route = MovieScreens.DetailsScreen.name + "/$movie")
+                }
+            }
+        }
+    }
+}
