523399d4334dde4f1028457a5e270b02ea743369
KamilNowakk <Kamil.nowak9@interia.pl>
2025-01-06 02:25:33 +0100

    Labolatorium 3 oraz 4

---
+++ app/src/main/java/com/example/labolatorium/navigation/MovieNavigation.kt
@@ -0,0 +1,31 @@
+package com.example.labolatorium.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.labolatorium.screens.detals.DetailsScreen
+import com.example.labolatorium.screens.home.HomeScreen
+
+@Preview
+@Composable
+fun MovieNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name) {
+        composable(MovieScreens.HomeScreen.name) {
+            HomeScreen(navController = navController)
+        }
+        composable(
+            MovieScreens.DetailsScreen.name + "/{movie}",
+            arguments = listOf(navArgument(name = "movie") { type = NavType.StringType })
+        ) { backStackEntry ->
+            DetailsScreen(
+                navController = navController,
+                movieData = backStackEntry.arguments?.getString("movie")
+            )
+        }
+    }
+}
